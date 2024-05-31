752ef964abd66f711e998467dbf5ceee62485537
Ryan Williams <ryan.j.williams@charter.com>
2024-05-31 15:33:10 -0600

    adding layers for nodejs lambda

--- lib/cdk-deploy-nodejs-lambda-stack.ts
+++ lib/cdk-deploy-nodejs-lambda-stack.ts
@@ -1,11 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Code, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class CdkDeployNodejsLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const layer = new LayerVersion(
+      this,
+      "Layer",
+      {
+        code: Code.fromAsset("./lib/layers"),
+        compatibleRuntimes: [ Runtime.NODEJS_LATEST ],
+        layerVersionName: "NodeJsLayer"
+      }
+    )
 
     const nodejsFunction = new NodejsFunction(
       this,
@@ -14,7 +24,15 @@ export class CdkDeployNodejsLambdaStack extends Stack {
         entry: "lib/lambdas/nodeLambdaFunction.ts",
         handler: "handler",
         memorySize: 128,
-        runtime: Runtime.NODEJS_LATEST
+        runtime: Runtime.NODEJS_LATEST,
+        layers: [
+          layer
+        ],
+        bundling: {
+          externalModules: [
+            'uuid'
+          ]
+        }
       }
     )
   }
--- lib/lambdas/nodeLambdaFunction.ts
+++ lib/lambdas/nodeLambdaFunction.ts
@@ -1,5 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
 exports.handler = async function(event: any) {
   console.log("request:", JSON.stringify(event));
+  console.log("Generating uuid: ", uuidv4());
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
